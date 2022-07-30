3bf18c749cad3ccf7dc36949c9e9af99474dae56
Rafi Siddique <rafisiddique67@gmail.com>
2022-07-30 19:37:54 +0600

    problem solved

--- hw 5/fibonacci_number.c
+++ hw 5/fibonacci_number.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int fibonacci(int n) {
-    if(n==1 || n==0) return n;
-    return ((n-1)+(n-2));
+    int a[39];
+    
+    a[0] = 0;
+    a[1] = 1;
+
+    for(int i=2; i<39; ++i) {
+        a[i] = a[i-1]+a[i-2];
+    }
+
+    for(int i=0; i<39; ++i) {
+        if(a[i]>n) break;
+        printf("%lld ", a[i]);
+    }
 }
 
 int main() {
@@ -11,7 +22,7 @@ int main() {
 
     scanf("%d", &n);
 
-    printf("%d\n", fibonacci(n));
+    fibonacci(n);
 
     return 0;
 
