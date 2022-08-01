fc6afdc3a0ed1975da21ea99359eacaec49313d6
Rafi Siddique <rafisiddique67@gmail.com>
2022-08-01 18:03:09 +0600

    solution has been changed

--- midterm/separate_prime_number_from_array.c
+++ midterm/separate_prime_number_from_array.c
@@ -13,8 +13,30 @@ int main() {
     for(int i=0; i<n; ++i) scanf("%d", &a[i]);
 
     for(int i=0; i<n; ++i) {
-        
+        if(a[i]==1) {
+            a[i] = 0;
+            continue;
+        }
+        for(int j=2; j<a[i]; ++j) {
+            if(a[i]%j==0) a[i] = 0;
+        }
     }
+
+    int count = 0;
+
+    for(int i=0; i<n; ++i) {
+        if(a[i]!=0) ++count;
+    }
+
+    int prime[count], index = 0;
+
+    for(int i=0; i<n; ++i) {
+        if(a[i]!=0) prime[index++] = a[i];
+    }
+
+    for(int i=0; i<count; ++i) printf("%d ", prime[i]);
+
+    printf("\n");
 
     return 0;
 
