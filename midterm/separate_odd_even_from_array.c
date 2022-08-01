ecf83460c32095de7b2e5f00e1bde600a8b433d0
Rafi Siddique <rafisiddique67@gmail.com>
2022-08-01 16:20:41 +0600

    problem solved

--- midterm/separate_odd_even_from_array.c
+++ midterm/separate_odd_even_from_array.c
@@ -2,13 +2,20 @@
 
 int main() {
 
-    int a[100], even[50], odd[50];
+    int n;
 
-    for(int i=0, j=1; i<100; ++i, ++j) a[i] = j;
+    printf("enter size of an array:\n");
+    scanf("%d", &n);
+
+    int a[n], even[n/2], odd[n/2];
+
+    printf("enter %d data:\n", n);
+
+    for(int i=0; i<n; ++i) scanf("%d", &a[i]);
 
     int index = 0;
 
-    for(int i=0; i<100; ++i) {
+    for(int i=0; i<n; ++i) {
         if(a[i]%2!=0) {
             odd[index++] = a[i];
         }
@@ -16,15 +23,20 @@ int main() {
 
     index = 0;
 
-    for(int i=0; i<100; ++i) {
+    for(int i=0; i<n; ++i) {
         if(a[i]%2==0) {
             even[index++] = a[i];
         }
     }
 
-    for(int i=0; i<50; ++i) printf("%d ", odd[i]);
+    printf("Printing odd numbers:\n");
+
+    for(int i=0; i<n/2; ++i) printf("%d ", odd[i]);
     printf("\n");
-    for(int i=0; i<50; ++i) printf("%d ", even[i]);
+
+    printf("Printing even numbers\n");
+
+    for(int i=0; i<n/2; ++i) printf("%d ", even[i]);
     printf("\n");
 
     return 0;
