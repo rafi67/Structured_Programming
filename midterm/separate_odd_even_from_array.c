ff51d6557224d4c11059bb766e5cb25efea0745a
Rafi Siddique <rafisiddique67@gmail.com>
2022-08-01 23:32:22 +0600

    problem solved

--- midterm/separate_odd_even_from_array.c
+++ midterm/separate_odd_even_from_array.c
@@ -2,22 +2,29 @@
 
 int main() {
 
-    int n;
+    int n, even = 0, odd = 0;
 
     printf("enter size of an array:\n");
     scanf("%d", &n);
 
-    int a[n], even[n/2], odd[n/2];
+    int a[n];
 
-    printf("enter %d data:\n", n);
+    printf("enter data for array:\n");
 
     for(int i=0; i<n; ++i) scanf("%d", &a[i]);
+
+    for(int i=0; i<n; ++i) {
+        if(a[i]%2!=0) ++odd;
+        else ++even;
+    }
+
+    int eve[even], od[odd];
 
     int index = 0;
 
     for(int i=0; i<n; ++i) {
         if(a[i]%2!=0) {
-            odd[index++] = a[i];
+            od[index++] = a[i];
         }
     }
 
@@ -25,18 +32,18 @@ int main() {
 
     for(int i=0; i<n; ++i) {
         if(a[i]%2==0) {
-            even[index++] = a[i];
+            eve[index++] = a[i];
         }
     }
 
     printf("Printing odd numbers:\n");
 
-    for(int i=0; i<n/2; ++i) printf("%d ", odd[i]);
+    for(int i=0; i<odd; ++i) printf("%d ", od[i]);
     printf("\n");
 
     printf("Printing even numbers\n");
 
-    for(int i=0; i<n/2; ++i) printf("%d ", even[i]);
+    for(int i=0; i<even; ++i) printf("%d ", eve[i]);
     printf("\n");
 
     return 0;
