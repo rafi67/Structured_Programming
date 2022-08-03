8192a2c62388cdd20cfb9dfdfc97e7a4a7af98f2
Rafi Siddique <rafisiddique67@gmail.com>
2022-08-03 15:59:58 +0600

    name changed

--- midterm/u_from_2_array.c
+++ midterm/u_from_2_array.c
@@ -11,29 +11,43 @@ void sort(int a[], int size) {
         }
     }
 }
+ 
 
 int main() {
 
-    int a1[5], a2[5], a[10], index = 0, count = 0;
+    int a1[5], a2[5], element_number = 0;
 
+    int a[10];
+    
     for(int i=0; i<5; ++i) scanf("%d", &a1[i]);
     for(int i=0; i<5; ++i) scanf("%d", &a2[i]);
 
+    int count = 0, index;
+
+    for(int i=0; i<5; ++i) {
+        a[i] = a1[i];
+        ++count;
+    }
+
+    index = count;
+
     for(int i=0; i<5; ++i) {
         for(int j=0; j<5; ++j) {
-            if(a1[i]==a2[j] && (a1[i]!=0 && a2[j]!=0)) {
-                a[index++] = a1[i];
-                a1[i] = 0;
-                a2[j] = 0;
-                ++count;
-            }
+            if(a[i]==a2[j]) a2[j] = 0;
+        }
+    }
+
+    for(int i=0; i<5; ++i) {
+        if(a2[i]!=0) {
+            a[index++] = a2[i];
+            ++count; 
         }
     }
 
     sort(a, count);
 
-    for(int i=0; i<count; ++i) printf("%d ", a[i]);
-    printf("\n");
+    for(int i=0; i<count; ++i) printf("%d ", a[i]); 
+
 
     return 0;
 
