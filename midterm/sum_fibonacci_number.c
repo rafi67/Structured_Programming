d7d9494a4d6287e70bea9afc1a328b60afb63040
Rafi Siddique <rafisiddique67@gmail.com>
2022-08-04 13:37:48 +0600

    solution updated

--- midterm/sum_fibonacci_number.c
+++ midterm/sum_fibonacci_number.c
@@ -2,15 +2,21 @@
 
 int main() {
 
-    int n, result = 0;
+    int a[39], n;
     scanf("%d", &n);
-
-    for(int i=0; i<n; ++i) {
-        if(i==0 || i==1) result += i;
-        else result += i-1;
+    
+    a[0] = 0;
+    a[1] = 1;
+    
+    for(int i=2; i<n; ++i) {
+        a[i] = a[i-1]+a[i-2];
     }
-
-    printf("Sum of fibonacci series until %dth position is %d\n", n, result);
+    
+    for(int i=0; i<n; ++i) {
+        if(i!=0) a[i] = a[i-1]+a[i];
+    }
+    
+    printf("%d\n", a[n-1]);
 
     return 0;
 
